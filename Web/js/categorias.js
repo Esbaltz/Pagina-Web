551e5c63b7666f0dd214041d318303d11a7b03d2
Elías <el.antican@duocuc.cl>
2024-05-20 14:51:20 -0400

    cambios-formato

--- Web/js/categorias.js
+++ Web/js/categorias.js
@@ -1,31 +1,31 @@
 function toggleCategory() {
-    var fictionCategories = document.getElementById('fiction-categories');
-    var nonfictionCategories = document.getElementById('nonfiction-categories');
-    var toggleBtn = document.querySelector('.toggle-btn');
+  var fictionCategories = document.getElementById("fiction-categories");
+  var nonfictionCategories = document.getElementById("nonfiction-categories");
+  var toggleBtn = document.querySelector(".toggle-btn");
 
-    if (fictionCategories.style.display === 'none') {
-        fictionCategories.style.display = 'block';
-        nonfictionCategories.style.display = 'none';
-        toggleBtn.textContent = 'Ficción';
-    } else {
-        fictionCategories.style.display = 'none';
-        nonfictionCategories.style.display = 'block';
-        toggleBtn.textContent = 'No Ficción';
-    }
+  if (fictionCategories.style.display === "none") {
+    fictionCategories.style.display = "block";
+    nonfictionCategories.style.display = "none";
+    toggleBtn.textContent = "Ficción";
+  } else {
+    fictionCategories.style.display = "none";
+    nonfictionCategories.style.display = "block";
+    toggleBtn.textContent = "No Ficción";
+  }
 }
 
 function toggleCategory2() {
-    var fictionCategories = document.getElementById('fiction-categories');
-    var nonfictionCategories = document.getElementById('nonfiction-categories');
-    var toggleBtn = document.querySelector('.toggle-btn');
+  var fictionCategories = document.getElementById("fiction-categories");
+  var nonfictionCategories = document.getElementById("nonfiction-categories");
+  var toggleBtn = document.querySelector(".toggle-btn");
 
-    if (nonfictionCategories.style.display === 'none') {
-        nonfictionCategories.style.display = 'block';
-        fictionCategories.style.display = 'none';
-        toggleBtn.textContent = 'No Ficción';
-    } else {
-        nonfictionCategories.style.display = 'none';
-        fictionCategories.style.display = 'block';
-        toggleBtn.textContent = 'Ficción';
-    }
+  if (nonfictionCategories.style.display === "none") {
+    nonfictionCategories.style.display = "block";
+    fictionCategories.style.display = "none";
+    toggleBtn.textContent = "No Ficción";
+  } else {
+    nonfictionCategories.style.display = "none";
+    fictionCategories.style.display = "block";
+    toggleBtn.textContent = "Ficción";
+  }
 }
